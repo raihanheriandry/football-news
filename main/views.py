982f07dcce747beaada0aca0916f1a02c2f000f8
raihanheriandry <raihanmaulana.heriandry@gmail.com>
2025-09-17 13:58:39 +0700

    Update context in show_main

--- main/views.py
+++ main/views.py
@@ -22,9 +22,9 @@ def show_main(request):
         news_list = News.objects.filter(user=request.user)
 
     context = {
-        'npm': '240123456',
+        'npm': '2406351024',
         'name': request.user.username,
-        'class': 'PBP A',
+        'class': 'PBP E',
         'news_list': news_list,
         'last_login': request.COOKIES.get('last_login', 'Never')
     }
